b002211b127f690aa995a37558e4a341e1607b4a
Jack Shedd <jshedd@thisismess.com>
2012-10-24 04:10:32 -0500

    Removing .DS_Store files

--- benchmark/__init__.py
+++ benchmark/__init__.py
@@ -1,11 +1,11 @@
 __author__ = "Jeffrey R. Spies"
 __copyright__ = "Copyright 2012 Jeffrey R. Spies"
-__credits__ = ["Jeffrey R. Spies"]
+__credits__ = ["Jeffrey R. Spies", "Jack Shedd"]
 __license__ = "Apache License, Version 2.0"
 __VERSION__ = '0.1.5'
-__maintainer__ = "Jeffrey R. Spies"
-__email__ = "jspies@gmail.com"
-__status__ = "Beta"
+__maintainer__ = "Jack Shedd"
+__email__ = "jshedd@thisismess.com"
+__status__ = "Alpha"
 
 from main import BenchmarkProgram, main
 from Benchmark import Benchmark
